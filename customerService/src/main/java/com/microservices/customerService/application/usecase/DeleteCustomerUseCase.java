ba75c8454a9f62bb58d73c618f53463dddb5e328
Daniel Rodriguez <dangelsfocus@gmail.com>
2026-02-27 12:23:39 -0500

    [feat]: CRUD customer/person

---
+++ customerService/src/main/java/com/microservices/customerService/application/usecase/DeleteCustomerUseCase.java
@@ -0,0 +1,26 @@
+package com.microservices.customerService.application.usecase;
+
+import com.microservices.customerService.domain.exception.CustomerAlreadyExistsException;
+import com.microservices.customerService.domain.exception.CustomerNotFoundException;
+import com.microservices.customerService.domain.model.CustomerId;
+import com.microservices.customerService.domain.port.CustomerRepositoryPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteCustomerUseCase {
+    private final CustomerRepositoryPort repository;
+
+    public DeleteCustomerUseCase(CustomerRepositoryPort repository) {
+        this.repository = repository;
+    }
+
+    public void execute(String customerId) {
+        CustomerId validationId = CustomerId.of(customerId);
+
+        if (!repository.existsByCustomerId(validationId)) {
+            throw new CustomerNotFoundException(validationId);
+        }
+
+        repository.deleteByCustomerId(validationId);
+    }
+}
--- customerService/src/main/java/com/microservices/customerService/infrastructure/web/controller/CustomerController.java
+++ customerService/src/main/java/com/microservices/customerService/infrastructure/web/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.microservices.customerService.infrastructure.web.controller;
 import com.microservices.customerService.application.dto.CreateCustomerCommand;
 import com.microservices.customerService.application.dto.UpdateCustomerCommand;
 import com.microservices.customerService.application.usecase.CreateCustomerUseCase;
+import com.microservices.customerService.application.usecase.DeleteCustomerUseCase;
 import com.microservices.customerService.application.usecase.ListCustomersUseCase;
 import com.microservices.customerService.application.usecase.UpdateCustomerUseCase;
 import com.microservices.customerService.infrastructure.web.advice.ApiResponse;
@@ -12,6 +13,8 @@ import com.microservices.customerService.infrastructure.web.mapper.CustomerWebMa
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,17 +27,20 @@ public class CustomerController {
     private final CreateCustomerUseCase createCustomer;
     private final CustomerWebMapper mapper;
     private final UpdateCustomerUseCase updateCustomer;
+    private final DeleteCustomerUseCase deleteCustomer;
 
     public CustomerController(
             ListCustomersUseCase listCustomers,
             CustomerWebMapper mapper,
             CreateCustomerUseCase createCustomer,
-            UpdateCustomerUseCase updateCustomer
+            UpdateCustomerUseCase updateCustomer,
+            DeleteCustomerUseCase deleteCustomer
     ) {
         this.listCustomers = listCustomers;
         this.mapper = mapper;
         this.createCustomer = createCustomer;
         this.updateCustomer = updateCustomer;
+        this.deleteCustomer = deleteCustomer;
     }
 
     @GetMapping
@@ -44,7 +50,7 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ApiResponse<?> create(@RequestBody CreateCustomerRequest body,  HttpServletRequest req) {
+    public ResponseEntity<ApiResponse<?>>  create(@RequestBody CreateCustomerRequest body,  HttpServletRequest req) {
         var cmd = new CreateCustomerCommand(
                 body.getCustomerId(),
                 body.getPassword(),
@@ -59,11 +65,11 @@ public class CustomerController {
 
         var saved = createCustomer.execute(cmd);
         var response = mapper.toResponse(saved);
-        return ApiResponse.ok(response, req.getRequestURI());
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(response, req.getRequestURI()));
     }
 
     @PutMapping("/{customerId}")
-    public ApiResponse<?> update(
+    public ResponseEntity<ApiResponse<?>> update(
             @PathVariable String customerId,
             @Valid @RequestBody UpdateCustomerRequest body,
             HttpServletRequest req
@@ -83,6 +89,19 @@ public class CustomerController {
         var saved = this.updateCustomer.execute(cmd);
         var response = mapper.toResponse(saved);
 
-        return ApiResponse.ok(response, req.getRequestURI());
+        return ResponseEntity.ok(ApiResponse.ok(response, req.getRequestURI()));
     }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<?> delete(
+            @PathVariable String customerId,
+            HttpServletRequest req
+    ) {
+        if (customerId == null) throw new IllegalArgumentException("customerId is null");
+
+        deleteCustomer.execute(customerId);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
